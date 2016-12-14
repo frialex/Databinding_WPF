ba50e9e10df3f21dee37c5c06453a78ac383166a
Frison B Alexander <fri.b.alex@gmail.com>
2016-12-14 11:47:20 -0500

    Basic example
    of a databind from a C# file

---
+++ Databinding/ViewModels/Example.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Databinding.ViewModels
+{
+    public class Example
+    {
+
+        public string HeaderMessage { get; set; }
+
+        public List<OfThese> mooos = new List<OfThese>();
+
+
+        public Example()
+        {
+               HeaderMessage = "hi from a c# file";
+        }
+
+
+        public void ChangePage()
+        {
+            System.Diagnostics.Debugger.Launch();
+        }
+        
+        
+    }
+
+    public class OfThese
+    {
+        public string Name { get; set; }
+
+        
+        public void Moo()
+        {
+            Console.WriteLine($"One of them ({Name}) says hi");
+        }
+    }
+}
